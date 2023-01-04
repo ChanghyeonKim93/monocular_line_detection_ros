450d9cc505eb0efbc97d46baec41fa79f496a57e
ChanghyeonKim93 <hyun91015@gmail.com>
2023-01-04 23:37:57 +0900

    line detection OK.

--- include/monocular_line_detector_ros.h
+++ include/monocular_line_detector_ros.h
@@ -2,14 +2,20 @@
 #define _MONOCULAR_LINE_DETECTOR_ROS_H_
 
 #include <iostream>
-#include "ros/ros.h"
 
+// ROS related
+#include "ros/ros.h"
 #include "sensor_msgs/Image.h" // for 'image'
 
+// OpenCV related
+#include "cv_bridge/cv_bridge.h"
 #include "opencv2/core.hpp"
 
-#include "cv_bridge/cv_bridge.h"
-
+#include "opencv2/ximgproc/fast_line_detector.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/ximgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
 
 class MonocularLineDetectorROS
 {
@@ -19,7 +25,6 @@ private:
 // Subscribers
 private:
     ros::Subscriber sub_image_;
-
     std::string topicname_image_;
 
 // Publishers
@@ -29,6 +34,7 @@ private:
 // CLASS of the YOUR OWN MODULE
 private:
     // YourLibrary yl_;
+    cv::Ptr<cv::ximgproc::FastLineDetector> fast_line_detector_;
 
 public:
     MonocularLineDetectorROS(const ros::NodeHandle& nh);
--- src/monocular_line_detector_ros.cpp
+++ src/monocular_line_detector_ros.cpp
@@ -22,7 +22,30 @@ MonocularLineDetectorROS::MonocularLineDetectorROS(const ros::NodeHandle& nh)
     
 
     // Construct YOUR library
-    // 
+    // Create FLD detector
+    // Param               Default value   Description
+    // length_threshold    10            - Segments shorter than this will be discarded
+    // distance_threshold  1.41421356    - A point placed from a hypothesis line
+    //                                     segment farther than this will be
+    //                                     regarded as an outlier
+    // canny_th1           50            - First threshold for
+    //                                     hysteresis procedure in Canny()
+    // canny_th2           50            - Second threshold for
+    //                                     hysteresis procedure in Canny()
+    // canny_aperture_size 3            - Aperturesize for the sobel operator in Canny().
+    //                                     If zero, Canny() is not applied and the input
+    //                                     image is taken as an edge image.
+    // do_merge            false         - If true, incremental merging of segments
+    //                                     will be performed
+    int   length_threshold = 20;
+    float distance_threshold = 1.41421356f;
+    double canny_th1 = 40.0;
+    double canny_th2 = 80.0;
+    int canny_aperture_size = 3; // sobel filter size
+    bool do_merge = false;
+    this->fast_line_detector_ = cv::ximgproc::createFastLineDetector(length_threshold,
+            distance_threshold, canny_th1, canny_th2, canny_aperture_size,
+            do_merge);
 
     // run
     this->run();
@@ -48,12 +71,30 @@ void MonocularLineDetectorROS::callbackImage(const sensor_msgs::ImageConstPtr& m
     cv_bridge::CvImagePtr cv_ptr;
     cv_ptr = cv_bridge::toCvCopy(msg, msg->encoding); 
 
-    cv::Mat img = cv_ptr->image;
-    ROS_INFO_STREAM("Size : " << img.cols << ", " << img.rows);
+    cv::Mat img_gray,img_color;
+    cv_ptr->image.copyTo(img_color);
+    cv::cvtColor(img_color, img_gray, CV_BGR2GRAY);
+    img_gray.convertTo(img_gray, CV_8UC1);
+    
+    // ROS_INFO_STREAM("Size : " << img_gray.cols << ", " << img_gray.rows);
 
-    // YOUR algorithm !
-    // Do Yourself
+    // Detect lines on image.
+    std::vector<cv::Vec4f> lines;
+    fast_line_detector_->detect(img_gray, lines);
 
+    ROS_INFO_STREAM("# of detected lines: " << lines.size() ); // 1.5 ms
+
+    // Image showing
+    for(const cv::Vec4f& line : lines)
+    {
+        cv::Point2f p0(line(0),line(1));
+        cv::Point2f p1(line(2),line(3));
+        cv::line(img_color, p0,p1, cv::Scalar(255,0,255),2);
+    }
+
+    cv::namedWindow("img with lines");
+    cv::imshow("img with lines", img_color);
+    cv::waitKey(5);
 
     // Done.
 };
